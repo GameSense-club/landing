116e7578092228deefa6a90000d0f2a395458a03
falbue <cyansair05@gmail.com>
2025-08-10 01:07:55 +0500

    Указана верная страница с акциями

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/promotions')
+def index():
+    return render_template("promotions.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5001)
