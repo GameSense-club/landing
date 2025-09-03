f4fce0bf20f3ace733cae4f4b0885bf7827d9779
Koj10 <misteotimer@gmail.com>
2025-09-03 19:30:07 +0500

    Добавил реквизиты

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def index():
 def promotions():
     return render_template("promotions.html")
 
+@app.route('/privacy')
+def privacy():
+    return render_template("privacy.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5001)
